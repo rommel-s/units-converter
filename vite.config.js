c85fe8711c9bf4ad07b9aa6b56a5f1dae0e215cf
Rommel Santos <53845558+rommel-s@users.noreply.github.com>
2023-03-26 18:35:04 -0300

    Finished pwa build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Conversor de Unidades",
+        short_name: "Conversor de Unidades",
+        start_url: ".",
+        display: "standalone",
+        theme_color: "#2BBBAD",
+        background_color: "#2BBBAD",
+        icons: [
+          {
+            src: "/assets/icons/512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/assets/icons/192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+          {
+            src: "/assets/icons/144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
