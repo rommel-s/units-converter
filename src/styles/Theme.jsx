28424295432569b45f6d915b1e43a870f45b27f5
Rommel Santos <53845558+rommel-s@users.noreply.github.com>
2023-03-26 18:03:15 -0300

    Updated theme colors

--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -1,28 +1,28 @@
 export const theme = {
   colors: {
     main: {
-      primary: "#2BBBAD"
+      primary: "#2BBBAD",
     },
     shades: {
       darken01: "#26A694",
       darken02: "#009688",
       darken03: "#00796B",
-      darken04: "#004D40"
+      darken04: "#004D40",
     },
     tints: {
       lighten01: "#80CBC4",
       lighten02: "#B2DFDB",
-      lighten03: "#E0F2F1"
+      lighten03: "#E0F2F1",
     },
     neutral: {
-      white: "#26A694",
-      black: "#26A694",
-      gray: "#B0B0B0"
-    }
+      white: "#ffffff",
+      black: "#000000",
+      gray: "#B0B0B0",
+    },
   },
   fonts: {
     light: 300,
     regular: 400,
-    bold: 700
-  }
+    bold: 700,
+  },
 };
