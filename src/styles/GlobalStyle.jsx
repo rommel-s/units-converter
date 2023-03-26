06f6b9ee6a44bf3c1bcd6e283c92778586c61288
Rommel Santos <53845558+rommel-s@users.noreply.github.com>
2023-03-26 16:24:57 -0300

    Added styles

---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,27 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto Condensed', sans-serif;
+    /* border: 1px solid red; */
+  }
+
+  ::-webkit-scrollbar {
+    width: 0px;
+  }
+
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type="number"] {
+    -moz-appearance: textfield;
+  }
+
+`;
---
+++ src/styles/Theme.jsx
@@ -0,0 +1,28 @@
+export const theme = {
+  colors: {
+    main: {
+      primary: "#2BBBAD"
+    },
+    shades: {
+      darken01: "#26A694",
+      darken02: "#009688",
+      darken03: "#00796B",
+      darken04: "#004D40"
+    },
+    tints: {
+      lighten01: "#80CBC4",
+      lighten02: "#B2DFDB",
+      lighten03: "#E0F2F1"
+    },
+    neutral: {
+      white: "#26A694",
+      black: "#26A694",
+      gray: "#B0B0B0"
+    }
+  },
+  fonts: {
+    light: 300,
+    regular: 400,
+    bold: 700
+  }
+};
